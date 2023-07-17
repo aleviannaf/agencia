16f4e858062dbd3a009e163f84de2e5c15836928
aleviannaf <alexandrevianna511@gmail.com>
2023-07-17 18:25:02 -0400

    feat: class error config

---
+++ src/middlewares/handleErrors.middlewares.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express'
+import { AppError, NotFound } from '../error'
+
+const handleError = (
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Response => {
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({ message: error.message })
+    }
+
+    if (error instanceof NotFound) {
+        return res.status(404).json({ message: error.message })
+    }
+
+    console.error(error)
+    return res.status(500).json({ message: 'Internal Server Error.' })
+}
+
+export { handleError }
